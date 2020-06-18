1fe52312aa990198901af7bbecac483528a1b3b8
Scofield Hsu <scofield.hsu@gmail.com>
2020-06-18 18:07:29 +0800

    use class ScofieldHelper

--- app/src/main/java/scofield/orz/scofieldtts/MainActivity.java
+++ app/src/main/java/scofield/orz/scofieldtts/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ScofieldHelper mHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHelper = new ScofieldHelper(this);
     }
 }
